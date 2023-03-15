a0fa40e97c75f4b67316f5be01cbc75f85a311f3
yachuh <yachuh@gmail.com>
2023-03-15 18:50:46 +0800

    added header

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,26 @@
-console.log('Hello!');
+console.log("Hello!");
 
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  console.log("HesSchool Hello!");
 });
+
+//=== mobile menu ===//
+const headerNav = document.querySelector(".headerNav");
+const mobileNav = document.querySelector(".mobileNav");
+const menuIcon = document.querySelector(".menuIcon");
+const closeIcon = document.querySelector(".closeIcon");
+
+function toggleMenu() {
+  if (headerNav.classList.contains("showMenu")) {
+    headerNav.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    headerNav.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+  console.log("click");
+  // headerNav.classList.toggle("");
+}
+mobileNav.addEventListener("click", toggleMenu);
